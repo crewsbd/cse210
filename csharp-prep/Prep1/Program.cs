216fb94d9ad4d08a2ea4bde447104e811abbce61
crewsbd <bdcrews@protonmail.com>
2023-01-01 02:50:01 -0800

    W01 Prove: Developer—C# Programming Parts 1–2

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,23 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World! I changed this!");
+        Console.Write("What is your first name? ");
+        string firstName = Console.ReadLine();
+        Console.Write("What is your last name? ");
+        string lastName = Console.ReadLine();
+
+        Console.WriteLine($"Your name is {lastName}, {firstName} {lastName}.");
+
+        /*Console.Write("What is your name? ");
+        string name = Console.ReadLine();
+        string[] names = name.Split(" ");
+        if(names.Length >= 2)
+        {
+            Console.WriteLine($"Your name is {names[1]}, {names[0]} {names[1]}.");
+        }
+        else
+        {
+            Console.WriteLine($"Hello {name}.");
+        } */
     }
 }
--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,57 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        //Get grade percentage from the user and convert it to an integer
+        Console.Write("Grade percentage: ");
+        string stringGradePercentage = Console.ReadLine();
+        int intGradePercentage = (int)float.Floor(float.Parse(stringGradePercentage));
+        int gradeLastDigit = intGradePercentage % 10;
+        string letter = "F";
+     
+        //Determine letter grade
+        if(intGradePercentage >= 90)
+        {
+            letter = "A";
+        }
+        else if(intGradePercentage >= 80)
+        {
+            letter = "B";
+        }
+        else if(intGradePercentage >= 70)
+        {
+            letter = "C";
+        }
+        else if(intGradePercentage >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        //Add letter grade modifier
+        if(gradeLastDigit >= 7 && intGradePercentage < 90 && intGradePercentage >= 60)
+        {
+            letter = letter + "+";
+        }
+        else if(gradeLastDigit < 3 && intGradePercentage >= 60)
+        {
+            letter = letter + "-";
+        }
+
+        //Print letter grade
+        Console.WriteLine($"Grade: {letter}");
+
+        //Print feedback
+        if(intGradePercentage >= 70)
+        {
+            Console.WriteLine("You are to be congratulated on passing this course.");
+        }
+        else
+        {
+            Console.WriteLine("We regret to inform you that you have not passed this class. Keep working hard and you'll get it next time.");
+        }
+
     }
 }
