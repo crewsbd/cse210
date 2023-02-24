b7e1d65ec5adb3ef95e7d8206173e4e90b3999ab
crewsbd <bdcrews@protonmail.com>
2023-02-24 14:22:05 -0800

    Replaced strings array with new StringPool inst..

--- prove/Develop04/ReflectionActivity.cs
+++ prove/Develop04/ReflectionActivity.cs
@@ -3,25 +3,25 @@ public class ReflectionActivity : Activity
     //private string[] _messages;
     private string[] _messages;
     private string[] _prompts;
-    private List<int> _unusedMessages;
-    private List<int> _unusedPrompts;
-    private Random _randomNumber;
+    private StringPool _messagePool;
+    private StringPool _promptPool;
+
+
     public ReflectionActivity()
     {
         _name = "Reflecting Activity";
         _description = "This activity will help you reflect on times in your life when you have shown strength and resilience. This will help you recognize the power you have and how you can use it in other aspects of your life.";
         
-        _messages = new string[]
+        _messagePool = new StringPool( new string[]
         {
             "Think of a time when you stood up for someone else.",
             "Think of a time when you did something really difficult.",
             "Think of a time when you helped someone in need.",
             "Think of a time when you did something truly selfless."
-        };
-        _unusedMessages = new List<int>();
-        ResetUnusedMessages();
-
-        _prompts = new string[]
+        }
+        );
+        _promptPool = new StringPool(
+            new string[]
         {
             "Why was this experience meaningful to you?",
             "Have you ever done anything like this before?",
@@ -32,12 +32,8 @@ public class ReflectionActivity : Activity
             "What could you learn from this experience that applies to other situations?",
             "What did you learn about yourself through this experience?",
             "How can you keep this experience in mind in the future?"
-        };
-        _unusedPrompts = new List<int>();
-        ResetUnusedPrompts();
-
-        _randomNumber = new Random(DateTime.Today.Millisecond);
-        
+        }
+        );  
     }
 
     protected override void RunActivity() //Unique activity code
@@ -56,23 +52,5 @@ public class ReflectionActivity : Activity
 
         } while (durationCounter > 0);
 
-    }
-    private void ResetUnusedMessages()
-    {
-        
-        _unusedMessages.Clear();
-        for(int i = 0; i <= _messages.Count(); i++)
-        {
-            _unusedMessages.Add(i);
-        }
-    }
-        private void ResetUnusedPrompts()
-    {
-        
-        _unusedPrompts.Clear();
-        for(int i = 0; i <= _prompts.Count(); i++)
-        {
-            _unusedPrompts.Add(i);
-        }
     }
 }
--- prove/Develop04/StringPool.cs
+++ prove/Develop04/StringPool.cs
@@ -1,11 +1,33 @@
+
 public class StringPool 
 {
     private List<string> _pool;
     private List<int> _unused;
     private Random _randomNumber;
 
+    public StringPool()
+    {
+        _pool = new List<string>();
+        _unused = new List<int>();
+        _randomNumber = new Random(DateTime.Now.Millisecond);
+    }
+    public StringPool(string[] newStringArray)
+    {
+        _pool = new List<string>(newStringArray);
+        ResetUnused();
+    }
+
     public string getRandom()
     {
         return "";
+    }
+    private void ResetUnused()
+    {
+        
+        _unused.Clear();
+        for(int i = 0; i <= _pool.Count(); i++)
+        {
+            _unused.Add(i);
+        }
     }
 }
