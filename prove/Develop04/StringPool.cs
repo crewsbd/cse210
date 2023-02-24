f7b55dcd4fcf15ca59503d43dc44cfb8a90d8a6f
crewsbd <bdcrews@protonmail.com>
2023-02-24 14:01:53 -0800

    Refactoring pool of random messages

---
+++ prove/Develop04/StringPool.cs
@@ -0,0 +1,11 @@
+public class StringPool 
+{
+    private List<string> _pool;
+    private List<int> _unused;
+    private Random _randomNumber;
+
+    public string getRandom()
+    {
+        return "";
+    }
+}
