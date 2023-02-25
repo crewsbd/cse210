81b470b6d3e1cce72aa70a389f23c27c0527438f
crewsbd <bdcrews@protonmail.com>
2023-02-24 19:13:59 -0800

    Listing activity now uses StringPool

--- prove/Develop04/ListingActivity.cs
+++ prove/Develop04/ListingActivity.cs
@@ -1,20 +1,18 @@
 public class ListingActivity : Activity
 {
-    private string[] _prompts;
-    private Random _randomNumber;
+    private StringPool _promptPool;
     public ListingActivity()
     {
         _name = "Listing Activity";
         _description = "This activity will help you reflect on the good things in your life by having you list as many things as you can in a certain area.";
-        _prompts = new string[]
+        _promptPool = new StringPool( new string[]
         {
             "Who are people that you appreciate?",
             "What are personal strengths of yours?",
             "Who are people that you have helped this week?",
             "When have you felt the Holy Ghost this month?",
             "Who are some of your personal heroes?"
-        };
-        _randomNumber = new Random(DateTime.Today.Millisecond);
+        });
     }
 
     protected override void RunActivity() //Unique activity code
@@ -23,7 +21,7 @@ public class ListingActivity : Activity
         long newTime;
 
         int durationCounter = _duration;
-        Console.Write($"List as many responses as you can to the following prompt:\n --- {_prompts[_randomNumber.Next(_prompts.Count())-1]} ---\nYou may begin in:\n");
+        Console.Write($"List as many responses as you can to the following prompt:\n --- {_promptPool.GetRandom()} ---\nYou may begin in:\n");
         CountDown(5);
         do
         {
