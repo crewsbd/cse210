4452b6795f047037a20bad407b6d1ed4a6e0e31d
crewsbd <bdcrews@protonmail.com>
2023-03-16 15:27:54 -0700

    Put on infinte loop. SCREEN SAVER!

--- final/FinalProject/Program.cs
+++ final/FinalProject/Program.cs
@@ -18,20 +18,23 @@ class Program
 | 12                             |
 |--------------------------------|";
         TextImage buffer = new TextImage(box);
-       
+
         TextImage image = new TextImage(@"/---\
 |   |
 \---/");
 
-        for(int ctr = 0; ctr < 10; ctr++)
+        do
         {
-            Thread.Sleep(75);
-            buffer.Draw(box,0,0);
-            buffer.Draw(image.GetString(), ctr, ctr );
-            Console.Clear();
-            Console.WriteLine(buffer.GetString());
+            for (int ctr = 0; ctr < 10; ctr++)
+            {
+                Thread.Sleep(75);
+                buffer.Draw(box, 0, 0);
+                buffer.Draw(image.GetString(), ctr, ctr);
+                Console.Clear();
+                Console.WriteLine(buffer.GetString());
 
-        }
-        
+            }
+        } while (true); //Just testing. 
+
     }
 }
