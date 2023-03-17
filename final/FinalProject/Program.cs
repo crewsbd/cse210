db5b5d4994254890e93e0a5d27920e365c1d53f4
crewsbd <bdcrews@protonmail.com>
2023-03-17 01:21:44 -0700

    Card outline drawing

--- final/FinalProject/Program.cs
+++ final/FinalProject/Program.cs
@@ -4,31 +4,31 @@ class Program
 {
     static void Main(string[] args)
     {
-        string box = @"|--------------------------------|
-| 2                              |
-| 3                              |
-| 4                              |
-| 5                              |
-| 6                              |
-| 7                              |
-| 8                              |
-| 9                              |
-| 10                             |
-| 11                             |
-| 12                             |
-|--------------------------------|";
-        TextImage buffer = new TextImage(box);
-
-        TextImage image = new TextImage(@"/---\
-|   |
-\---/");
+        TextImage buffer = new TextImage(70, 23);
+        TextImage image = new TextImage(5, 4);
+        image.DrawCard(0,0,5,4);
 
         do
         {
-            for (int ctr = 0; ctr < 10; ctr++)
+            
+            int cardWidth = 20;
+            int cardHeight = 7;
+
+            for (int ctr = 0; ctr < 30; ctr++)
             {
                 Thread.Sleep(75);
-                buffer.Draw(box, 0, 0);
+                buffer.DrawCard(0, 0, 70, 23, false); //clear buffer
+                for(int x = 0; x < 3; x++)
+            {
+                for(int y = 0; y < 3; y++)
+                {
+                    buffer.DrawCard(x * cardWidth + 1, y * cardHeight + 1, cardWidth, cardHeight, false);
+                }
+            }
+                //buffer.Draw(box, 0, 0);
+                
+                //buffer.Draw(image.GetString(), ctr, ctr);
+                buffer.DrawCard(ctr, 4, 5, 4, false);
                 buffer.Draw(image.GetString(), ctr, ctr);
                 Console.Clear();
                 Console.WriteLine(buffer.GetString());
--- final/FinalProject/TextImage.cs
+++ final/FinalProject/TextImage.cs
@@ -1,27 +1,94 @@
 public class TextImage
 {
     private string[] _textArray;
-    public TextImage(string imageString)
+    private int _width, _height;
+    public TextImage(int width, int height)
     {
-        _textArray = imageString.Split("\n");
+        _width = width;
+        _height = height;
+        _textArray = new string[_height];
+        for (int y = 0; y < _height; y++)
+        {
+            _textArray[y] = "".PadLeft(_width, ' ');
+        }
     }
     public void Draw(string text, int x, int y)
     {
         string[] splitText = text.Split("\n");
         int ty = y; //Target y index
-        for (int sy = 0; sy < splitText.Count(); sy++) //Source y index
+        for (int sy = 0; sy < (splitText.Count() <= _height ? splitText.Count() : _height); sy++) //Source y index
         {
             if (ty > _textArray.Count()) //Break if target x exceeds target height
                 break;
-            if (ty >= 0)
+            if (ty >= 0 && ty < _textArray.Count()) //Only draw if index in range
             {
-                int firstLength = x;
-                int secondLength = _textArray[ty].Length - (x + splitText[sy].Length);
-
-                _textArray[ty] = _textArray[ty].Substring(0, firstLength) + splitText[sy] + _textArray[ty].Substring(x + splitText[sy].Length, secondLength);
+                if (x < _width) //If it doesn't start outside width
+                {
+                    //_textArray[ty] = _textArray[ty].Remove(x, splitText[sy].Length); //take out
+                    _textArray[ty] = _textArray[ty].Remove(x, (splitText[sy].Length + x < _width ? splitText[sy].Length : _width - x));
+                    _textArray[ty] = _textArray[ty].Insert(x, splitText[sy]); //put in
+                    if (_textArray[ty].Length > _width)
+                    {
+                        _textArray[ty] = _textArray[ty].Substring(0, _width);
+                    }
+                }
             }
             ty++;
         }
+    }
+    public void DrawCard(int x, int y, int width, int height, Boolean stack = false)
+    {
+        string img = "";
+        for (int ly = y; ly < y + height + 1; ly++)
+        {
+            if (ly == y)
+            {
+                //fist line
+                img += $"╭{"".PadLeft(width - 2, '─')}╮\n";
+            }
+            else if (ly == y + height - 1)
+            {
+                //last line
+                img += $"╰{"".PadLeft(width - 2, '─')}╯";
+                if (stack)
+                {
+                    img += "│\n";
+                }
+                else
+                {
+                    img += "\n";
+                }
+            }
+            else if (ly == y + height)
+            {
+                //stack effect on bottom
+                if(stack)
+                {
+                img += $" ╰{"".PadLeft(width - 2, '─')}╯";
+                }
+            }
+            else
+            {
+                //middle lines
+                img += $"│{"".PadLeft(width - 2, ' ')}";
+                if (stack)
+                {
+                    if (ly == y + 1)
+                    {
+                        img += "├╮\n";
+                    }
+                    else
+                    {
+                        img += "││\n";
+                    }
+                }
+                else
+                {
+                    img += "│\n";
+                }
+            }
+        }
+        Draw(img, x, y);
     }
     public string GetString()
     {
