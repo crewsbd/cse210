4c2be1062c668bd6ec29ba617e1530239977c767
crewsbd <bdcrews@protonmail.com>
2023-03-26 17:09:46 -0700

    Wrapped text helper

---
+++ final/FinalProject/Helpers.cs
@@ -0,0 +1,28 @@
+public static class Helpers
+{
+    static public int ReadInt()
+    {
+        int val;
+        while(!int.TryParse(Console.ReadLine(),out val)){
+            Console.Write("Invalid input. Try again:");
+        }
+        return val;
+    }
+    static public string WrapText(string sourceText, int targetLength)
+    {
+        string wrappedText = "";
+        string[] splitString = sourceText.Split(" ");
+        for(int word = 0; word < splitString.Count(); word++)
+        {
+            if((wrappedText+splitString[word]).Length > targetLength)
+            {
+                wrappedText += $"\n{splitString[word] }";
+            }
+            else
+            {
+            wrappedText += $"\n{splitString[word] }";
+            }
+        }
+        return wrappedText;
+    }
+}
